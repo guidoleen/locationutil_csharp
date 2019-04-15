8135d81451ffc1e14a10bc876589076fca696dd3
Guido Leen <guidoleen@live.nl>
2019-04-15 16:34:39 +0200

    Aangepaste verie

---
+++ LocationUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationUtil
+{
+    public class LocationUtils
+    {
+        public bool isEmpty(String strInput)
+        {
+            if (strInput == "" || strInput == null)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
